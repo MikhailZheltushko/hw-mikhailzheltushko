a0108983b14b27796061aacf1e910f6acb7b3348
MikhailZheltushko <ZheltushkoMike@mail.ru>
2024-09-20 11:08:04 +0300

    HW4 is completed

--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -2,31 +2,165 @@ package main
 
 import "fmt"
 
-func printChessboard(size int) {
-	for i := 0; i < size; i++ {
-		for j := 0; j < size; j++ {
-			if (i+j)%2 == 0 {
-				fmt.Print("# ")
-			} else {
-				fmt.Print("  ")
-			}
-		}
-		fmt.Println()
+type ComparisonMode struct {
+	ComparisonType ComparisonType
+}
+
+type ComparisonType int
+
+const (
+	ByYear ComparisonType = iota
+	BySize
+	ByRate
+)
+
+type Book struct {
+	id     int
+	title  string
+	author string
+	year   int
+	size   int
+	rate   float64
+}
+
+func NewComparisonMode(compareBy ComparisonType) *ComparisonMode {
+	return &ComparisonMode{
+		ComparisonType: compareBy,
+	}
+}
+
+func (c ComparisonMode) CompareBooks(book1, book2 *Book) bool {
+	switch c.ComparisonType {
+	case ByYear:
+		return book1.year > book2.year
+	case BySize:
+		return book1.size > book2.size
+	case ByRate:
+		return book1.rate > book2.rate
+	default:
+		return false
+	}
+}
+
+func NewBook(title string, author string, year int, size int, rate float64) *Book {
+	return &Book{
+		title:  title,
+		author: author,
+		year:   year,
+		size:   size,
+		rate:   rate,
 	}
 }
 
 func main() {
-	fmt.Println("Пример шахмотной доски 8х8")
-	size := 8
-
-	printChessboard(size)
-
-	fmt.Println("Можно создать доску любого размера, введите размер доски: ")
-	_, err := fmt.Scanf("%d", &size)
-	if err != nil {
-		fmt.Println("Ошибка ввода размера: ", err)
-		return
+	myBook := Book{
+		id:     1,
+		title:  "Go Programming",
+		author: "John Doe",
+		year:   2021,
+		size:   300,
+		rate:   4.5,
 	}
+	fmt.Println("\n====================================================")
+	fmt.Println("Вывод информации о книге ДО использования методов")
+	fmt.Println("Book ID:", myBook.id)
+	fmt.Println("Title:", myBook.title)
+	fmt.Println("Author:", myBook.author)
+	fmt.Println("Year:", myBook.year)
+	fmt.Println("Size:", myBook.size)
+	fmt.Println("Rate:", myBook.rate)
+	fmt.Println("====================================================")
 
-	printChessboard(size)
+	fmt.Println("\nИспользуя метод GetingID, получили значение id:", myBook.GetingID())
+	myBook.SetingID(2)
+	fmt.Println("Используя метод SetingID, обновили значание id на:", myBook.GetingID())
+
+	fmt.Println("\nИспользуя метод GetingTitle, получили значение title:", myBook.GetingTitle())
+	myBook.SetingTitle("Golang Programming")
+	fmt.Println("Используя метод SetingTitle, обновили значание title на:", myBook.GetingTitle())
+
+	fmt.Println("\nИспользуя метод GetingAuthor, получили значение author:", myBook.GetingAuthor())
+	myBook.SetingAuthor("Женек Дой")
+	fmt.Println("Используя метод SetingAuthor, обновили значание author на:", myBook.GetingAuthor())
+
+	fmt.Println("\nИспользуя метод GetingYear, получили значение year:", myBook.GetingYear())
+	myBook.SetingYear(2024)
+	fmt.Println("Используя метод SetingYear, обновили значание year на:", myBook.GetingYear())
+
+	fmt.Println("\nИспользуя метод GetingSize, получили значение size:", myBook.GetingSize())
+	myBook.SetingSize(426)
+	fmt.Println("Используя метод SetingSize, обновили значание size на:", myBook.GetingSize())
+
+	fmt.Println("\nИспользуя метод GetingRate, получили значение rate:", myBook.GetingRate())
+	myBook.SetingRate(4.3)
+	fmt.Println("Используя метод SetingRate, обновили значание rate на:", myBook.GetingRate())
+
+	fmt.Println("\n====================================================")
+	fmt.Println("Вывод информации о книге ПОСЛЕ использования методов")
+	fmt.Println("Book ID:", myBook.id)
+	fmt.Println("Title:", myBook.title)
+	fmt.Println("Author:", myBook.author)
+	fmt.Println("Year:", myBook.year)
+	fmt.Println("Size:", myBook.size)
+	fmt.Println("Rate:", myBook.rate)
+	fmt.Println("====================================================")
+
+	book1 := NewBook("Book 1", "Author 1", 2020, 350, 4.2)
+	book2 := NewBook("Book 2", "Author 2", 2021, 360, 4.0)
+
+	mode := NewComparisonMode(ByRate)
+
+	if mode.CompareBooks(book1, book2) {
+		fmt.Println("Book 1 is greater than Book 2")
+	} else {
+		fmt.Println("Book 2 is greater than Book 1")
+	}
+}
+
+func (b *Book) SetingID(newID int) {
+	b.id = newID
+}
+
+func (b *Book) GetingID() int {
+	return b.id
+}
+
+func (b *Book) SetingTitle(newTitle string) {
+	b.title = newTitle
+}
+
+func (b *Book) GetingTitle() string {
+	return b.title
+}
+
+func (b *Book) SetingAuthor(newAuthor string) {
+	b.author = newAuthor
+}
+
+func (b *Book) GetingAuthor() string {
+	return b.author
+}
+
+func (b *Book) SetingYear(newYear int) {
+	b.year = newYear
+}
+
+func (b *Book) GetingYear() int {
+	return b.year
+}
+
+func (b *Book) SetingSize(newSize int) {
+	b.size = newSize
+}
+
+func (b *Book) GetingSize() int {
+	return b.size
+}
+
+func (b *Book) SetingRate(newRate float64) {
+	b.rate = newRate
+}
+
+func (b *Book) GetingRate() float64 {
+	return b.rate
 }
